68563969ad43de889ea52fa11c144c5245ca112f
lhirdman <love.hirdman@gmail.com>
2015-07-10 13:46:53 +0200

    The java

---
+++ talkRcon.java
@@ -0,0 +1,37 @@
+import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
+import com.github.koraktor.steamcondenser.steam.servers.GoldSrcServer;
+import java.util.concurrent.TimeoutException;
+
+public class talkRcon {
+    public void getPlayerList() {
+        
+    }
+    public void main(String[] args) {
+        GoldSrcServer server = null;
+        try {
+            server = new GoldSrcServer("hede.nifelheim.ws", 32330);
+        } catch (SteamCondenserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            server.initialize();
+        } catch (SteamCondenserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(server.getPlayers());
+        } catch (SteamCondenserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
+
